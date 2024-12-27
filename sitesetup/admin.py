ec35aa5b1e5ef42468bf58dfe14e70f0ee2e6c7d
Allan San <allanterzisanches@gmail.com>
2024-12-27 17:46:10 -0300

    Added SiteSetup and TabularInLine model and admin

--- sitesetup/admin.py
+++ sitesetup/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from sitesetup.models import MenuLink
+from sitesetup.models import MenuLink, SiteSetup
+
+
+class MenuLinkInline(admin.TabularInline):
+    model = MenuLink
+    extra = 1
+
 
 @admin.register(MenuLink)
 class MenuLinkAdmin(admin.ModelAdmin):
     list_display = ('id', 'text', 'url_or_path')
     list_display_links = ('id', 'text', 'url_or_path')
     search_fields = ('id', 'text', 'url_or_path')
+
+
+@admin.register(SiteSetup)
+class SiteSetup(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    inlines = MenuLinkInline,
