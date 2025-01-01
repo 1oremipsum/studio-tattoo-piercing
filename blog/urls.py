bf522431efb23e80a829d29047f94f3e6670e415
Allan San <allanterzisanches@gmail.com>
2025-01-01 20:38:20 -0300

    Creating the necessary templates for the layout

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'), 
+    path('galeria', views.gallery, name='gallery'),
+    path('post', views.post, name='post'), 
+    path('page', views.page, name='page'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'blog/pages/index.html')
+
+
+def gallery(request):
+    return render(request, 'blog/pages/gallery.html')
+
+
+def page(request):
+    return render(request, 'blog/pages/page.html')
+
+
+def post(request):
+    return render(request, 'blog/pages/post.html')
