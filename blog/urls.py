e9a5e7bf5bc53efc23dd19f1ef111ad027e4470d
Allan San <allanterzisanches@gmail.com>
2025-01-06 21:24:34 -0300

    Blog layout

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index'), 
+    path('', views.index, name='index'),
+    path('blog', views.home_blog, name='home_blog'),
     path('galeria', views.gallery, name='gallery'),
-    path('post', views.post, name='post'), 
-    path('page', views.page, name='page'),
+    path('post', views.post, name='post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,27 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+posts = list(range(1000))
 
 def index(request):
     return render(request, 'blog/pages/index.html')
+
+
+def home_blog(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'blog/pages/blog.html', { 'page_obj': page_obj })
 
 
 def gallery(request):
     return render(request, 'blog/pages/gallery.html')
 
 
-def page(request):
-    return render(request, 'blog/pages/page.html')
-
-
 def post(request):
-    return render(request, 'blog/pages/post.html')
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'blog/pages/post.html',)
