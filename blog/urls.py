61141f6a20ac72db7294abffe80ab75c6a71877f
Allan San <allanterzisanches@gmail.com>
2025-01-19 21:16:00 -0300

    Creating URLs and links for content filtering (author, category) inside the Post.

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('blog/', views.home_blog, name='home_blog'),
     path('galeria/', views.gallery, name='gallery'),
     path('post/<slug:slug>/', views.post, name='post'),
+    path('created_by/<int:author_id>/', views.created_by, name='created_by'),
+    path('category/<slug:slug>/', views.category, name='category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -14,9 +14,7 @@ def index(request):
 
 def home_blog(request):
     posts =  Post.objects.get_published()
-    
     page_obj = pagination(request, posts)
-
     title = SiteSetup.objects.all().first().title
 
     context = {
@@ -27,15 +25,43 @@ def home_blog(request):
     return render(request, 'blog/pages/blog.html', context)
 
 
-def gallery(request):
-    return render(request, 'blog/pages/gallery.html')
-
-
-def post(request):
+def created_by(request, author_id):
+    posts =  Post.objects.get_published().filter(created_by__pk=author_id)
+    page_obj = pagination(request, posts)
+    title = SiteSetup.objects.all().first().title
 
     context = {
-        'site_title': 'Studio Tattoo & Piercing | Post',
-        # 'page_obj': page_obj
+        'site_title': title,
+        'page_obj': page_obj
+    }
+
+    return render(request, 'blog/pages/blog.html', context)
+
+
+def category(request, slug):
+    posts =  Post.objects.get_published().filter(category__slug=slug)
+    page_obj = pagination(request, posts)
+    title = SiteSetup.objects.all().first().title
+
+    context = {
+        'site_title': title,
+        'page_obj': page_obj
+    }
+
+    return render(request, 'blog/pages/blog.html', context)
+
+
+def post(request, slug):
+    post =  Post.objects.get_published().filter(slug=slug).first()
+    title = SiteSetup.objects.all().first().title
+
+    context = {
+        'site_title': f"{post.title} - {title}",
+        'post': post,
     }
 
     return render(request, 'blog/pages/post.html', context)
+
+
+def gallery(request):
+    return render(request, 'blog/pages/gallery.html')
